7b94bf81e1792921ceed980c8b86dd8f106bd119
VG-Huai <56375896+VG-Huai@users.noreply.github.com>
2021-05-24 16:53:06 +0800

    Update memtrace.c
    baseline implementation

--- task2/memtrace.c
+++ task2/memtrace.c
@@ -24,11 +24,14 @@ static void  (*old_free_hook)(void* ptr, const void* caller);
 static void *(*old_realloc_hook)(void *ptr, size_t size, const void *caller);
 
 #define BACK_TRACE_DEPTH 8
-#define CACHE_SIZE       10
+#define CACHE_SIZE       512
+#define FLUSH_SIZE       256
 
-// static clock_t start_time;
+static long leak_time_threshold = 2000;
 static FILE* g_memory_trace_fp = NULL;
 static int   g_memory_trace_cache_used = 0;
+static int   g_memory_trace_cache_start = 0;
+static int   g_memory_trace_last_flush = 0;
 /*additional 3 items: alloc/free addr size*/
 static void* g_memory_trace_cache[CACHE_SIZE][BACK_TRACE_DEPTH + 4]; 
 static void  memory_trace_flush(void);
@@ -66,7 +69,7 @@ static void memory_trace_init(void)
     if(g_memory_trace_fp == NULL)
     {
         char file_name[260] = {0};
-        snprintf(file_name, sizeof(file_name), "./%d_memory.log", getpid());
+        snprintf(file_name, sizeof(file_name), "./logs/%d_memory.log", getpid());
         if((g_memory_trace_fp = fopen(file_name, "wb+")) != NULL)
         {
 
@@ -85,6 +88,8 @@ static void memory_trace_deinit(void)
     if(g_memory_trace_fp != NULL)
     {
         memory_trace_restore();
+        memory_trace_flush();
+        g_memory_trace_last_flush = 1;
         memory_trace_flush();
         fclose(g_memory_trace_fp);
         g_memory_trace_fp = NULL;
@@ -156,10 +161,10 @@ static void memory_trace_flush_one_entry(int index)
     {
         int i = 0;
         offset = strlen(buffer);
-        for (int j = 0; j < BACK_TRACE_DEPTH; j++){
-            printf(" %s ", symbols[j]);  
-        }  
-        printf("\n");
+        // for (int j = 0; j < BACK_TRACE_DEPTH; j++){
+        //     printf(" %s ", symbols[j]);  
+        // }  
+        // printf("\n");
         for(i = 0; i < BACK_TRACE_DEPTH; i++)
         {
             if(symbols[i] == NULL)
@@ -186,20 +191,77 @@ static void memory_trace_flush_one_entry(int index)
     return;
 }
 
+static void memory_leak_check(void){
+    int check_size = CACHE_SIZE;
+    if(g_memory_trace_last_flush == 0){
+        check_size = FLUSH_SIZE;
+    }
+    for(int i = 0; i < FLUSH_SIZE; i++)
+    {
+        int cur = (g_memory_trace_cache_start + i) % CACHE_SIZE;
+        int check = 1;
+        // check this memory is freed or not
+        if((int)g_memory_trace_cache[cur][BACK_TRACE_DEPTH] == 0){
+            continue;
+        }
+        for(int j = 1; j < check_size; j++){
+            int nex = (j + cur) % CACHE_SIZE;
+            // current address = next address, update status
+            
+            if((long)g_memory_trace_cache[nex][BACK_TRACE_DEPTH+1] == (long)g_memory_trace_cache[cur][BACK_TRACE_DEPTH+1]){
+                printf("%p and ",g_memory_trace_cache[cur][BACK_TRACE_DEPTH+1]);
+                printf("%p\n",g_memory_trace_cache[nex][BACK_TRACE_DEPTH+1]);
+                printf("check occurs \n");
+                // if time between two accesses is longer than 'leak_time_threshold', assert leak occurs, check = 1 and break;
+                if((long)g_memory_trace_cache[nex][BACK_TRACE_DEPTH+3] - (long)g_memory_trace_cache[cur][BACK_TRACE_DEPTH+3] >= leak_time_threshold){
+                    check = 1;
+                    break;
+                }
+                else{
+                    check = 0;
+                    break;
+                }
+                printf("check finishes \n");
+                // if allocated, check = 1, else if freed, check = 0
+                // if((int)g_memory_trace_cache[nex][BACK_TRACE_DEPTH] == 1){
+                //     check = 1;
+                // }
+                // else{
+                //     check = 0;
+                //     break;
+                // }
+            }
+        }
+        if(check == 1){
+            // Show backtrace information of leak part
+            printf("mem leak may occurs at: %p size = %d\nBacktrace information are shown here:", g_memory_trace_cache[cur][BACK_TRACE_DEPTH + 1], (int)g_memory_trace_cache[cur][BACK_TRACE_DEPTH + 2]);
+            void** backtrace_buffer = g_memory_trace_cache[cur];
+            char** symbols = backtrace_symbols(backtrace_buffer, BACK_TRACE_DEPTH);
+            for (int j = 0; j < BACK_TRACE_DEPTH; j++)
+            {
+            printf(" %s ", symbols[j]);  
+            }  
+            printf("\n");
+        }
+    }
+}
+
 static void memory_trace_flush(void)
 {
-    char buff[64] = {0x00};  
-    sprintf(buff,"cat /proc/%d/maps", getpid());    
     int fd = fileno(g_memory_trace_fp);
-    // system((const char*) buff);  
-    write(fd, buff, strlen(buff));
     int i = 0;
-    for(i = 0; i < g_memory_trace_cache_used; i++)
+    memory_leak_check();
+    for(i = g_memory_trace_cache_start; i < FLUSH_SIZE + g_memory_trace_cache_start; i++)
     {
         memory_trace_flush_one_entry(i);
     }
-    g_memory_trace_cache_used = 0;
-
+    g_memory_trace_cache_used = g_memory_trace_cache_used - FLUSH_SIZE;
+    if(g_memory_trace_cache_start > 0){
+        g_memory_trace_cache_start = 0;
+    }
+    else{
+        g_memory_trace_cache_start = FLUSH_SIZE;
+    }
     return;
 }
 
@@ -216,12 +278,12 @@ static void memory_trace_write(int alloc, void* addr, int size, long tp)
 
     for(i = 0; i < BACK_TRACE_DEPTH; i++)
     {
-        g_memory_trace_cache[g_memory_trace_cache_used][i] = backtrace_buffer[i];
+        g_memory_trace_cache[(g_memory_trace_cache_used + g_memory_trace_cache_start) % CACHE_SIZE][i] = backtrace_buffer[i];
     }
-    g_memory_trace_cache[g_memory_trace_cache_used][BACK_TRACE_DEPTH] = (void*)alloc;
-    g_memory_trace_cache[g_memory_trace_cache_used][BACK_TRACE_DEPTH+1] = addr;
-    g_memory_trace_cache[g_memory_trace_cache_used][BACK_TRACE_DEPTH+2] = (void*)size;
-    g_memory_trace_cache[g_memory_trace_cache_used][BACK_TRACE_DEPTH+3] = (void*)tp;
+    g_memory_trace_cache[(g_memory_trace_cache_used + g_memory_trace_cache_start) % CACHE_SIZE][BACK_TRACE_DEPTH] = (void*)alloc;
+    g_memory_trace_cache[(g_memory_trace_cache_used + g_memory_trace_cache_start) % CACHE_SIZE][BACK_TRACE_DEPTH+1] = addr;
+    g_memory_trace_cache[(g_memory_trace_cache_used + g_memory_trace_cache_start) % CACHE_SIZE][BACK_TRACE_DEPTH+2] = (void*)size;
+    g_memory_trace_cache[(g_memory_trace_cache_used + g_memory_trace_cache_start) % CACHE_SIZE][BACK_TRACE_DEPTH+3] = (void*)tp;
 
     g_memory_trace_cache_used++;
 
