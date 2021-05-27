836b52ff317ce6dda0404ffdea3c659e82ecab9b
VG-Huai <56375896+VG-Huai@users.noreply.github.com>
2021-05-27 20:50:18 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,16 +24,29 @@ int main() {
                 std::cin >> file_name;
                 fin.open(file_name);
             }
+            // task2/case1.cpp
+            std::string command = "g++ -g -rdynamic -o target ";
+            command = command + file_name;
+            command = command + " -Wl,-Map,target.map"; 
+            // std::cout << command <<"\n"<< std::endl;
+            system(command.data());
 
-            std::string line;
-            while (!fin.eof()) {
-                getline(fin, line);
-                std::cout << line << std::endl;
-            }
-
+            // std::string line;
+            // while (!fin.eof()) {
+            //     getline(fin, line);
+            //     std::cout << line << std::endl;
+            // }
+            const char* exec_file = "LD_PRELOAD=./task2/libmem.so ./target";
+            system(exec_file);
             fin.close();
 
-            sleep(5);
+            std::cout << "Input q or Q to quit: ";
+            std::string quit_input;
+            std::cin >> quit_input;
+            while (quit_input != "q" && quit_input != "Q") {
+                std::cout << "Input q or Q to quit: ";
+                std::cin >> quit_input;
+            }
         } else if (type == "q" || type == "Q") {
             return (0);
         }
